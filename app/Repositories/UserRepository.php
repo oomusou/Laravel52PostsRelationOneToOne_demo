2659e2c3c0c181da2f4a8b8d3750929642e70d26
oomusou <oomusou@gmail.com>
2016-01-04 10:56:25 +0800

    closure加上type hint : HasOne

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class UserRepository
 {
@@ -12,10 +13,11 @@ class UserRepository
      */
     public function getUserPost() : Collection
     {
-        return User::with(['post' => function ($query) {
+        return User::with(['post' => function (HasOne $query) {
                 $query->where('id', '<', '6');
             }])
             ->where('id', '>', 2)
             ->get();
+
     }
 }
